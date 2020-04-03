e9c093e1f126fb67d42a8cf6fe71fd9b380b48cd
Mikael Samvelian <mikael.samvelian@gmail.com>
2020-04-03 17:34:27 -0400

    Add trace file logic

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 from tweet import Tweet
 from n_gram import N_Gram
 from language import Language
+from collections import Counter
 
 size = 3
 vocab = 1
@@ -12,9 +13,6 @@ n_gram = N_Gram(in_file1, size, vocab, smoothing)
 language_count = n_gram.language_count
 
 tweets = []
-
-right = 0
-wrong = 0
 
 eu = Language('eu', uni['eu'], bi['eu'], tri['eu'])
 es = Language('es', uni['es'], bi['es'], tri['es'])
@@ -42,50 +40,60 @@ for line in input_file:
         print('ERROR reading file')
 input_file.close()
 
+
+overall_result = Counter()
+language_result = {'eu': Counter(), 'ca': Counter(), 'gl': Counter(), 'es': Counter(), 'en': Counter(), 'pt': Counter()}
 debug = 0
-for tweet in tweets:
-    try:
-        if vocab == 0:
-            tweet.lower_case()
-        elif vocab == 1:
-            tweet.case_sensitive()
-        elif vocab == 2:
-            tweet.is_alpha()
-        tweet.counter()
 
-        count = {}
-        if size == 1:
-            count = tweet.uni
-        elif size == 2:
-            count = tweet.bi
-        elif size == 3:
-            count = tweet.tri
+with open(f'ModifiedDataSet/trace_{vocab}_{size}_{smoothing}.txt','w') as csvfile:
+    for tweet in tweets:
+        try:
+            if vocab == 0:
+                tweet.lower_case()
+            elif vocab == 1:
+                tweet.case_sensitive()
+            elif vocab == 2:
+                tweet.is_alpha()
+            tweet.counter()
 
-        score = {}
+            count = {}
+            if size == 1:
+                count = tweet.uni
+            elif size == 2:
+                count = tweet.bi
+            elif size == 3:
+                count = tweet.tri
 
-        score['eu'] = eu.score(count, language_count)
-        score['es'] = es.score(count, language_count)
-        score['ca'] = ca.score(count, language_count)
-        score['gl'] = gl.score(count, language_count)
-        score['pt'] = pt.score(count, language_count)
-        score['en'] = en.score(count, language_count)
+            score = {}
 
-        estimate_l = max(score, key=score.get)
+            score['eu'] = eu.score(count, language_count)
+            score['es'] = es.score(count, language_count)
+            score['ca'] = ca.score(count, language_count)
+            score['gl'] = gl.score(count, language_count)
+            score['pt'] = pt.score(count, language_count)
+            score['en'] = en.score(count, language_count)
 
-        if debug < 0:
-            print(tweet.tweet_id)
-            print(score)
-            print(estimate_l, tweet.language)
+            estimate_l = max(score, key=score.get)
+            estimate_s = max(score, key = lambda s: score[s])
+            if debug < 0:
+                print(tweet.tweet_id)
+                print(score)
+                print(estimate_l, tweet.language)
 
-        if estimate_l == tweet.language:
-            right += 1
-        else:
-            wrong += 1
+            if estimate_l == tweet.language:
+                overall_result['right'] += 1
+                language_result[tweet.language]['right'] += 1
+            else:
+                overall_result['wrong'] += 1
+                language_result[tweet.language]['wrong'] += 1
 
-        debug += 1
-    except:
-        print('ERROR calculating score')
+                correct_wrong = 'correct' if estimate_l == tweet.language else 'wrong'
+                csvfile.write(f'{tweet.tweet_id}  {estimate_l}  {score[estimate_s]}  {tweet.language}  {correct_wrong}\n')
 
+            debug += 1
+        except:
+            print('ERROR calculating score')
 
-print('right: ', (right/(right+wrong))*100, '%')
-print('wrong: ', (wrong/(right+wrong))*100, '%')
+# print(language_result)
+print('right: ', (overall_result['right']/sum(overall_result.values()))*100, '%')
+print('wrong: ', (overall_result['wrong']/sum(overall_result.values()))*100, '%')
